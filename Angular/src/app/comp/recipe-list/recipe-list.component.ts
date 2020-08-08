2a805ff1d8d6967a201a7b3c9b06c93d90e6947e
Jacob <jacob.saini60@gmail.com>
2020-08-08 12:26:16 +1000

    Add fade animation to recipes

--- Angular/src/app/comp/recipe-list/recipe-list.component.ts
+++ Angular/src/app/comp/recipe-list/recipe-list.component.ts
@@ -1,16 +1,10 @@
-import { RecipeService} from '../../services/recipe-service.service'
+import { RecipeService } from '../../services/recipe-service.service';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { LoadingAnimService } from '../../services/loading/loading-anim.service'
+import { LoadingAnimService } from '../../services/loading/loading-anim.service';
 import { debounceTime } from 'rxjs/operators';
 
-import {
-  trigger,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
-
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-recipe-list',
@@ -18,84 +12,85 @@ import {
   styleUrls: ['./recipe-list.component.css'],
   animations: [
     trigger('fade', [
-      transition('void => in', [
-        style({ opacity: 0}),
-        animate(800, style({opacity:1}))
+      transition('out <=> in', [
+        style({ opacity: 0 }),
+        animate(1000, style({ opacity: 1 })),
       ]),
-      transition('in => out', [
-        animate('1s', style({ opacity:0}))
-      ])
-    ])
-  ]
+    ]),
+  ],
 })
 export class RecipeListComponent implements OnInit {
   loadingSub: Subscription;
-  loading:boolean = false;
+  loading: boolean = false;
   change = false;
-  ext:boolean = false;
+  animationTrigger: boolean = false;
+  ext: boolean = false;
   onclick: boolean = false;
-  ingreServ:boolean;
+  ingreServ: boolean;
   ingredients: String;
-  main:String; 
-  recipes:any;
+  main: String;
+  recipes: any;
   number: number;
-  diet:any =  '';
-  intol:any = '';
+  diet: any = '';
+  intol: any = '';
   exclude: any = '';
-  
 
-  
+  constructor(
+    private recipeService: RecipeService,
+    private loadingScreenServ: LoadingAnimService
+  ) {}
 
-  constructor(private recipeService: RecipeService, private loadingScreenServ: LoadingAnimService) { }
- 
   ngOnInit() {
-    this.loadingSub = this.loadingScreenServ.loadingStatus.pipe(
-      debounceTime(200)
-    ).subscribe((value) =>{
-      this.loading = value;
-    })
+    this.loadingSub = this.loadingScreenServ.loadingStatus
+      .pipe(debounceTime(200))
+      .subscribe((value) => {
+        this.loading = value;
+      });
   }
 
-
-  getByIngredients () {
+  getByIngredients() {
     // this.ingredients = this.ingredients.replace(/\s/g, '');
-    console.log(this.ingredients,this.number)
-    this.recipeService.getByIngredient(this.ingredients,this.number)
-    .subscribe(recipes => {
-      this.ingredients = '';
-      this.loading = true;
-      this.recipes = recipes;
-      console.log(recipes)
-      this.ingreServ = true;  
-    })
-    error => {
-      console.log(error)
-    }};
+    console.log(this.ingredients, this.number);
+    this.recipeService
+      .getByIngredient(this.ingredients, this.number)
+      .subscribe((recipes) => {
+        this.ingredients = '';
+        this.loading = true;
+        this.recipes = recipes;
+        this.ingreServ = true;
+        this.animationTrigger = !this.animationTrigger;
+      });
+    (error) => {
+      console.log(error);
+    };
+  }
 
-    getBySearch () {
-      console.log(this.main)
-      this.recipeService.getBySearch(this.main,this.number,this.diet,this.intol,this.exclude)
-      .subscribe(recipes => {
-       this.ingreServ = false;
+  getBySearch() {
+    console.log(this.main);
+    this.recipeService
+      .getBySearch(this.main, this.number, this.diet, this.intol, this.exclude)
+      .subscribe((recipes) => {
+        this.ingreServ = false;
         this.main = '';
         this.loading = true;
         this.recipes = recipes;
-        console.log(recipes) 
-      })
-      error => {
-        console.log(error)
-      }};
+        this.animationTrigger = !this.animationTrigger;
+      });
+    (error) => {
+      console.log(error);
+    };
+  }
 
-    ngOnDestroy(){
-      this.loadingSub.unsubscribe();
-    }
+  ngOnDestroy() {
+    this.loadingSub.unsubscribe();
+  }
 
-    changeSearch(){ 
-     this.change = !this.change;
-    }
+  changeSearch() {
+    this.change = !this.change;
+  }
 
-    extra(){
-      this.ext = true
-      this.onclick = !this.onclick;
-    }
+  extra() {
+    this.ext = true;
+    this.onclick = !this.onclick;
+  }
 }
