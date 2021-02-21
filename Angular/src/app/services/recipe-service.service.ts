968ebced56a5e2f67184da5c34ebfc340abb570e
Jacob <jacob.saini60@gmail.com>
2021-02-21 18:05:54 +1000

    heroku test

--- Angular/src/app/services/recipe-service.service.ts
+++ Angular/src/app/services/recipe-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import * as env from "../../../conf.js";
 
 const baseUrl = process.env.API_URL;
 
